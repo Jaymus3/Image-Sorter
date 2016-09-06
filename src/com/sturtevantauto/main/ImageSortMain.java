11df2003bbcde220d842e12f7e5ad386fa06e2f4
James <zaleus3@gmail.com>
2016-09-06 15:29:41 -0500

    New main for direct image sorter execution
    This project is largely on hold due to the amount of cars we’re dealing
    with here right now.  This allows me to bypass the login screen until
    everything is properly rigged up.

--- src/com/sturtevantauto/main/ImageSortMain.java
+++ src/com/sturtevantauto/main/ImageSortMain.java
@@ -1,34 +1,25 @@
+/**
+ * 
+ * @author Aevum Kairos
+ * @Note
+ * Javadoc authored by me doesn't necessarily indicate that I wrote it. It just means I documented the purpose of something.
+ */
 package com.sturtevantauto.main;
 
 import java.awt.AWTException;
 import java.awt.EventQueue;
-import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Scanner;
-
-import javax.swing.JOptionPane;
 import javax.swing.UIManager;
-
-import com.sturtevantauto.gui.LoginWindow;
 import com.sturtevantauto.gui.MainGUI;
-import com.sturtevantauto.io.CarDefinitions;
-import com.sturtevantauto.io.ImageInterface;
-import com.sturtevantauto.io.Logger;
-import com.sturtevantauto.io.MakeModelInterface;
 
-@SuppressWarnings("unused")
-public class Main {
-    /**
-     * Runs at the start of the program and sets properties as well as
-     * initializing the GUI.
-     * 
-     * @param args
-     * @throws IOException
-     * @throws AWTException
-     * @throws ClassNotFoundException
-     * @throws SQLException
-     */
+/**
+ *
+ * @author Aevum Kairos
+ * @Note
+ * Javadoc authored by me doesn't necessarily indicate that I wrote it. It just means I documented the purpose of something.
+ */
+public class ImageSortMain {
     public static void main(String[] args) throws IOException, AWTException, ClassNotFoundException, SQLException {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -36,12 +27,13 @@ public class Main {
                     System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Image Sorter");
                     System.setProperty("apple.laf.useScreenMenuBar", "true");
                     UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                    LoginWindow window = new LoginWindow();
-                    window.frmLoginWindow.setVisible(true);
+                    MainGUI imagesort = new MainGUI();
+                    imagesort.frmImageSorter.setVisible(true);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         });
     }
+
 }
