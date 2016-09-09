9042d5ca074f06edb2bf36e65a4e54ce45aff704
James <zaleus3@gmail.com>
2016-09-09 10:26:16 -0500

    Maven Sunfire, prices are exported to command line
    Pricing is in a very early functionality state.  Still progress that
    needs to be made.

--- src/com/sturtevantauto/gui/PricingTool.java
+++ src/com/sturtevantauto/gui/PricingTool.java
@@ -18,7 +18,9 @@ public class PricingTool {
     private JFrame pricingFrame;
     private JTextField searchField;
     public JComboBox<String> yearBox;
+    private JComboBox<String> makeBox;
     private boolean action;
+    private boolean action2;
 
     /**
      * Launch the application.
@@ -65,35 +67,47 @@ public class PricingTool {
         searchPrompt.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
         searchPrompt.setHorizontalAlignment(SwingConstants.LEFT);
         searchPrompt.setForeground(Color.GRAY);
-       
+
         String[] models = { "Select a model" };
         JComboBox<String> modelBox = new JComboBox(models);
+        modelBox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (action2 && action)
+                {
+                    int weight = PricingToolHandler.getWeightByCar(makeBox.getSelectedItem().toString(),
+                            modelBox.getSelectedItem().toString(), yearBox.getSelectedItem().toString());
+                    double stdweight = PricingToolHandler.convertToStandard(weight);
+                    double stdprice = PricingToolHandler.getPrice(stdweight);
+                    System.out.println("Price: " + stdprice);
+                }
+            }
+        });
         modelBox.setSelectedIndex(0);
         modelBox.setBounds(244, 118, 200, 27);
         pricingFrame.getContentPane().add(modelBox);
 
         String[] makes = { "Select a make" };
-        final JComboBox<String> makeBox = new JComboBox(makes);
+        makeBox = new JComboBox(makes);
         makeBox.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                if(action)
-                {
-                modelBox.removeAllItems();
-                modelBox.addItem("Select a model");
-                System.out.println(makeBox.getSelectedItem());
-                PricingToolHandler.getModelsByMake(makeBox.getSelectedItem().toString(), yearBox.getSelectedItem().toString(), modelBox);
+                if (action) {
+                    action2 = false;
+                    modelBox.removeAllItems();
+                    modelBox.addItem("Select a model");
+                    System.out.println(makeBox.getSelectedItem());
+                    PricingToolHandler.getModelsByMake(makeBox.getSelectedItem().toString(),
+                            yearBox.getSelectedItem().toString(), modelBox);
+                    action2 = true;
                 }
             }
         });
         /*
-        makeBox.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                modelBox.removeAllItems();
-                System.out.println(makeBox.getSelectedItem());
-                PricingToolHandler.getModelsByMake(makeBox.getSelectedItem().toString(), yearBox.getSelectedItem().toString(), modelBox);
-            }
-        });
-        */
+         * makeBox.addActionListener(new ActionListener() { public void
+         * actionPerformed(ActionEvent e) { modelBox.removeAllItems();
+         * System.out.println(makeBox.getSelectedItem());
+         * PricingToolHandler.getModelsByMake(makeBox.getSelectedItem().toString
+         * (), yearBox.getSelectedItem().toString(), modelBox); } });
+         */
         makeBox.setBounds(244, 83, 200, 27);
         pricingFrame.getContentPane().add(makeBox);
 
--- src/com/sturtevantauto/io/PricingToolHandler.java
+++ src/com/sturtevantauto/io/PricingToolHandler.java
@@ -90,7 +90,7 @@ public class PricingToolHandler {
                 if (mak.equals(make)) {
                     String model = rs.getString("Model");
                     modelBox.addItem(model);
-                }
+                } //TODO: $100 per 2000
             }
             long adding = System.currentTimeMillis();
             System.out.println("Item adding time: " + (adding - statementt));
@@ -104,5 +104,55 @@ public class PricingToolHandler {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+    
+    public static int getWeightByCar(String make, String model, String year)
+    {
+        int weight = 0;
+        long start = System.currentTimeMillis();
+        try {
+            Connection connection = car.getConnection();
+            long connect = System.currentTimeMillis();
+            System.out.println("Connect time: " + (connect - start));
+            Statement statement = connection.createStatement();
+            statement.executeQuery("USE car_weight");
+            ResultSet rs = statement.executeQuery("SELECT * FROM cars_" + year);
+            long statementt = System.currentTimeMillis();
+            System.out.println("Statement time: " + (statementt - connect));
+            while (rs.next()) {
+                String maka = rs.getString("Make");
+                if (maka.equals(make)) {
+                    String modele = rs.getString("Model");
+                    if(modele.equals(model))
+                        weight = Integer.parseInt(rs.getString("Weight"));
+                } //TODO: $100 per 2000
+            }
+            long adding = System.currentTimeMillis();
+            System.out.println("Weight lookup time: " + (adding - statementt));
+            rs.close();
+            statement.close();
+            long closingtime = System.currentTimeMillis();
+            System.out.println("Closing time: " + (closingtime - adding));
+            System.out.println("Total time: " + (closingtime - start));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return weight;
+    }
+    
+    public static double convertToStandard(int metricweight)
+    {
+        double standard = 0;
+        standard = metricweight / 0.45359237;
+        return standard;
+    }
+    
+    public static double getPrice(double standardweight)
+    {
+        double price = 0;
+        price = standardweight * 0.05;
+        return price;
     }
 }
